51dd9e75c7694ef0a551f13e122128d27654720f
RajGM <rajgmsocial19@gmail.com>
2020-10-24 16:35:34 +0530

    scraping done efficient way

--- printTest.py
+++ printTest.py
@@ -8,26 +8,13 @@ data = urllib.request.urlopen('https://www.google-melange.com/archive/gsoc/'+yea
 soup = BeautifulSoup(data)
 x = 0
 orgDone = 0
-f = open('2015Org.txt','w')
-"""
+f = open('2015TextOrg.txt','w')
+
 for li in soup.find_all('a'):
  f.write(li.text+"\n")
  print(li.text+"\n")
 f.close()
-"""
 
-for li in soup.find_all('a'):
- if x==1 and li.text == "Google Summer of Code":
-  x = 0
- elif li.text == "Google Summer of Code":
-  x = 1
- elif li.text == "Privacy Policy" and x==1:
-  x = 3
-  orgDone = 1
-
- if x==1 and li.text != "Google Summer of Code" and orgDone==0:
-  f.write(li.text+"\n")
-  print(li.text+"\n")
 
 f.close()
 
