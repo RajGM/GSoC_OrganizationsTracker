967c45e6c0bfe92306a8b847086bfb9e38c63283
RajGM <rajgmsocial19@gmail.com>
2020-12-17 00:18:28 +0530

    dataJson done

---
+++ dataCombine.py
@@ -0,0 +1,25 @@
+import json
+
+combinedOrg = {}
+
+for year in range(2016,2021):
+    strYear = str(year)
+    f = open(strYear+'Org.txt','r')
+    for x in f.readlines():
+        if x in combinedOrg.keys():
+            print("Key exists")
+            print(combinedOrg[x])
+            combinedOrg[x].append(strYear)
+        else:
+            print("Adding Key")
+            combinedOrg[x] = list()
+            combinedOrg[x].append(strYear)
+    f.close()  
+
+print(combinedOrg)
+
+y = json.dumps(combinedOrg)
+
+f = open("data.json","w")
+f.write(y)
+f.close()
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Index.js running");
+let jsonData;
+
+fetch("./data.json")
+.then(response => { 
+    return response.json();
+})
+.then(
+    data => console.log(data));
