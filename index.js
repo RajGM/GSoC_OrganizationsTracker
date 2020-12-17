ade6491cc768335d93306288b634e2e332c617da
RajGM <rajgmsocial19@gmail.com>
2020-12-17 12:24:25 +0530

    data displaying to screen

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ console.log("Index.js running");
 let jsonData;
 
 fetch("./data.json")
-.then(response => { 
-    return response.json();
-})
-.then(
-    data => console.log(data));
+    .then(response => {
+        return response.json();
+    })
+    .then(
+        data => {
+            jsonData = data;
+        }
+    );
+
+setTimeout(() => {
+    //console.log(jsonData);
+
+    for (const [key, value] of Object.entries(jsonData)) {
+        document.body.innerHTML += `${key}: ${value} <br>`;
+        //console.log(`${key}: ${value}`);
+    }
+
+}, 2000);
