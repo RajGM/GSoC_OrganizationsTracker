2375f6f103fd719067cc4b4ec5b159b77cb2fcad
RajGM <rajgmsocial19@gmail.com>
2020-12-19 10:00:16 +0530

    rendering slowtime

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log("Index.js running");
 let jsonData;
-
+let startYear = 2009;
+let endYear = 2021;
+let totalOrg;
 fetch("./data.json")
     .then(response => {
         return response.json();
@@ -11,12 +13,20 @@ fetch("./data.json")
         }
     );
 
-setTimeout(() => {
-    //console.log(jsonData);
-
-    for (const [key, value] of Object.entries(jsonData)) {
-        document.body.innerHTML += `${key}: ${value} <br>`;
-        //console.log(`${key}: ${value}`);
+for (let i = 0; i < 300; i++) {
+    document.getElementById("orgData").innerHTML += `<div class="orgName"></div>`;
+    for (let j = 0; j < endYear - startYear; j++) {
+        document.getElementById("orgData").innerHTML += `<div class="boxClass">${j}</div>`;
     }
+    document.getElementById("orgData").innerHTML += `<br>`;
+}
 
+/*
+setTimeout(() => {
+    for (const [key, value] of Object.entries(jsonData)) {
+        document.getElementById("orgData").innerHTML += `${key}: ${value} <br>`;
+    }
+    totalOrg = Object.keys(jsonData).length
+    console.log(totalOrg);
 }, 2000);
+*/
