33082904c123572d4a5a5ae0f01fc93188b75a17
RajGM <rajgmsocial19@gmail.com>
2020-12-19 11:19:44 +0530

    data presentation done formula needed to convert from year to Yes and no

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let jsonData;
 let startYear = 2009;
 let endYear = 2021;
 let totalOrg;
+let myTable = document.getElementById("myTable");
+let row1 = document.getElementById("row1");
+let rowBuild = 0;
 fetch("./data.json")
     .then(response => {
         return response.json();
@@ -13,20 +16,44 @@ fetch("./data.json")
         }
     );
 
-for (let i = 0; i < 300; i++) {
-    document.getElementById("orgData").innerHTML += `<div class="orgName"></div>`;
-    for (let j = 0; j < endYear - startYear; j++) {
-        document.getElementById("orgData").innerHTML += `<div class="boxClass">${j}</div>`;
+function buildCell(){
+    for(let i=1;i<=endYear-startYear;i++){
+        let currCell = row1.insertCell(i);
+        currCell.innerHTML = startYear+i-1;
     }
-    document.getElementById("orgData").innerHTML += `<br>`;
 }
 
-/*
-setTimeout(() => {
-    for (const [key, value] of Object.entries(jsonData)) {
-        document.getElementById("orgData").innerHTML += `${key}: ${value} <br>`;
+buildCell();
+
+function testBuild(totalOrg){
+    //console.log("totalOrg:"+totalOrg);
+    for(let i=0;i<totalOrg;i++){
+        let currRow = myTable.insertRow(rowBuild+1);
+        for(let j=0;j<=endYear-startYear;j++){
+            let currCell = currRow.insertCell(j);
+            if(startYear+j-1 == "2008"){
+            currCell.innerHTML = "Organization Organization";
+            }else{
+            currCell.innerHTML = "No";
+            }
+        }
     }
+}
+
+setTimeout(() => {
     totalOrg = Object.keys(jsonData).length
-    console.log(totalOrg);
+    //console.log(totalOrg);
+    testBuild(totalOrg);
+    console.log(typeof jsonData);
+    console.log(Object.keys(jsonData));
+    let currRow = 1;
+    for (const [key, value] of Object.entries(jsonData)) {
+        //document.getElementById("orgData").innerHTML += `${key}: ${value} <br>`;
+        myTable.rows[currRow].cells[0].innerHTML = key;
+        for(let i=0;i<value.length;i++){
+            //introduce some formula here to solve it
+            myTable.rows[currRow].cells[value[i]-startYear].innerHTML = "Yes";
+        }
+        currRow++;
+    }
 }, 2000);
-*/
