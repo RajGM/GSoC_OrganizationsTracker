a822ef7fe198f0f38a4603c4051eba6bcf7a9ab0
RajGM <rajgmsocial19@gmail.com>
2021-01-02 23:29:42 +0530

    setTimeout removed and changed to async await

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//console.log("Index.js running");
 let jsonData;
 let startYear = 2009;
 let endYear = 2021;
@@ -6,56 +5,59 @@ let totalOrg;
 let myTable = document.getElementById("myTable");
 let row1 = document.getElementById("row1");
 let rowBuild = 0;
-fetch("./newData.json")
-    .then(response => {
-        return response.json();
-    })
-    .then(
-        data => {
-            jsonData = data;
-        }
-    );
 
-function buildCell(){
-    for(let i=1;i<=endYear-startYear;i++){
+
+
+function buildCell() {
+    for (let i = 1; i <= endYear - startYear; i++) {
         let currCell = row1.insertCell(i);
-        currCell.innerHTML = startYear+i-1;
+        currCell.innerHTML = startYear + i - 1;
+    }
+}
+
+function testBuild(totalOrg) {
+    for (let i = 0; i < totalOrg; i++) {
+        let currRow = myTable.insertRow(rowBuild + 1);
+        for (let j = 0; j <= endYear - startYear; j++) {
+            let currCell = currRow.insertCell(j);
+            if (startYear + j - 1 == "2008") {
+                currCell.innerHTML = "Organization";
+            } else {
+                currCell.innerHTML = "No";
+            }
+        }
+        rowBuild++;
+    }
+}
+
+function finalCall() {
+    totalOrg = Object.keys(jsonData).length;
+    const sortedObject = Object.fromEntries(Object.entries(jsonData).sort());
+    testBuild(totalOrg);
+    let currRow = 1;
+    for (const [key, value] of Object.entries(sortedObject)) {
+        myTable.rows[currRow].cells[0].innerHTML = key;
+        for (let i = 0; i < value.length; i++) {
+            myTable.rows[currRow].cells[value[i] - startYear + 1].innerHTML = "Yes";
+        }
+        currRow++;
     }
 }
 
 buildCell();
 
-function testBuild(totalOrg){
-    //console.log("totalOrg:"+totalOrg);
-    for(let i=0;i<totalOrg;i++){
-        let currRow = myTable.insertRow(rowBuild+1);
-        for(let j=0;j<=endYear-startYear;j++){
-            let currCell = currRow.insertCell(j);
-            if(startYear+j-1 == "2008"){
-            currCell.innerHTML = "Organization Organization";
-            }else{
-            currCell.innerHTML = "No";
+async function callME() {
+    await fetch("./newData.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(
+            data => {
+                jsonData = data;
             }
-        }
-    }
+        );
+
+    finalCall();
 }
 
-setTimeout(() => {
-    totalOrg = Object.keys(jsonData).length;
-    //console.log(totalOrg);
-    const sortedObject = Object.fromEntries(Object.entries(jsonData).sort());
-    console.log(sortedObject);
-    testBuild(totalOrg);
-    //console.log(typeof jsonData);
-    //console.log(Object.keys(jsonData));
-    let currRow = 1;
-    for (const [key, value] of Object.entries(sortedObject)) {
-        //document.getElementById("orgData").innerHTML += `${key}: ${value} <br>`;
-        myTable.rows[currRow].cells[0].innerHTML = key;
-        for(let i=0;i<value.length;i++){
-            //introduce some formula here to solve it
-            myTable.rows[currRow].cells[value[i]-startYear+1].innerHTML = "Yes";
-        }
-        currRow++;
-    }
-}, 2000);
+callME();
