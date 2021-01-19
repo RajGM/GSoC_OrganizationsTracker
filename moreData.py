9ea7f9069db93257a25928de763af1fbaf660bec
RajGM <rajgmsocial19@gmail.com>
2021-01-19 23:55:21 +0530

    organization name and link are now fetched

--- moreData.py
+++ moreData.py
@@ -2,11 +2,13 @@ from bs4 import BeautifulSoup
 import re
 import urllib.request
 
-for year in range(2016,2021):
- strYear = str(year)
- data = urllib.request.urlopen('https://summerofcode.withgoogle.com/archive/'+strYear+'/organizations/').read()
- soup = BeautifulSoup(data,'html.parser')
- #print(soup.prettify())
- for li in soup.find_all('md-option'):
-  print(li.text)
-  
+#for year in range(2016,2021):
+strYear = str(2016)
+data = urllib.request.urlopen('https://summerofcode.withgoogle.com/archive/'+strYear+'/organizations/').read()
+soup = BeautifulSoup(data,'html.parser')
+
+li = soup.find("section")
+#print(  len(li.contents[1]) )
+for ele in li.contents[1].ul.find_all("li"):
+ print( ele["aria-label"] ) 
+ print( ele.a["href"] ) 
