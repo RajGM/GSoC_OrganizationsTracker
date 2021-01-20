0d1b30e65953197ba7bed9a308a7015694f8b709
RajGM <rajgmsocial19@gmail.com>
2021-01-20 09:56:49 +0530

    list and dict mutation are Good to go now

--- moreData.py
+++ moreData.py
@@ -7,8 +7,23 @@ strYear = str(2016)
 data = urllib.request.urlopen('https://summerofcode.withgoogle.com/archive/'+strYear+'/organizations/').read()
 soup = BeautifulSoup(data,'html.parser')
 
+listdict = []
 li = soup.find("section")
-#print(  len(li.contents[1]) )
 for ele in li.contents[1].ul.find_all("li"):
- print( ele["aria-label"] ) 
- print( ele.a["href"] ) 
+ newdict = {
+     "org":ele["aria-label"],
+     "link":"https://summerofcode.withgoogle.com"+ele.a["href"]
+ }
+ listdict.append(newdict)
+ 
+#print(listdict)
+for i in range(len(listdict)):
+    #print(listdict[i])
+    #print( type(listdict[i]) )
+    #print( listdict[i]["org"] )
+    listdict[i]["org"] = listdict[i]["year"] = "2016"
+    #print( listdict[i] )
+    #print(type(listdict[i]))
+    #for x in listdict[i]:
+     #print(type(x))
+     #x["year"] = "2016"
