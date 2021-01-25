f48384fe6ad39fa43b458670ea10452958a66f03
RajGM <rajgmsocial19@gmail.com>
2021-01-25 11:22:55 +0530

    All features are now working, user testing needs to done next

--- index.js
+++ index.js
@@ -42,38 +42,36 @@ function testBuild(totalOrg) {
 }
 
 function changeData(e) {
+    console.log(jsonData);
     console.log(e.target);
     let targetRow = e.target.parentNode.rowIndex;
     let targetCol = e.target.cellIndex;
-    let orgName = Object.keys(jsonData)[e.target.parentNode.rowIndex-1];
+    let orgName = Object.keys(jsonData)[e.target.parentNode.rowIndex - 1];
     let activeColcell = activeCol[orgName];
-    console.log("Row:"+targetRow);
-    console.log("Column:"+targetCol);
-    console.log("OrgName:"+orgName);
-    myTable.rows[targetRow].cells[activeColcell].setAttribute("bgcolor","white");
-    activeCol[orgName]=targetCol;
+    let orgYear = startYear + targetCol - 1;
+    console.log("Row:" + targetRow);
+    console.log("Column:" + targetCol);
+    console.log("OrgName:" + orgName);
+    console.log("Year:" + orgYear);
+    myTable.rows[targetRow].cells[activeColcell].setAttribute("bgcolor", "white");
+    activeCol[orgName] = targetCol;
     
-
-/*
-    var parsedInt = parseInt(value.years[i]);
-            if (parsedInt >= 2016) {
-                if(value.years.length == i+1){
-                    myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor","green");
-                }
-                myTable.rows[currRow].cells[endYear - startYear + 1].innerHTML = value[value.years[i]].tech;
-                myTable.rows[currRow].cells[endYear - startYear + 2].innerHTML = value[value.years[i]].topics;
-                myTable.rows[currRow].cells[endYear - startYear + 3].innerHTML = "<a href=" + value[value.years[i]].link + ">Link</a>";
-            } else {
-                if(value.years.length == i+1){
-                    myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor","blue");
-                }
-                myTable.rows[currRow].cells[endYear - startYear + 1].innerHTML = "--";
-                myTable.rows[currRow].cells[endYear - startYear + 2].innerHTML = "--";
-                myTable.rows[currRow].cells[endYear - startYear + 3].innerHTML = "--"
-            }
-*/
-
-
+    if (orgYear >= 2016 && jsonData[orgName][orgYear]) {
+        myTable.rows[targetRow].cells[targetCol].setAttribute("bgcolor", "green");
+        myTable.rows[targetRow].cells[endYear - startYear + 1].innerHTML = jsonData[orgName][orgYear].tech;
+        myTable.rows[targetRow].cells[endYear - startYear + 2].innerHTML = jsonData[orgName][orgYear].topics;
+        myTable.rows[targetRow].cells[endYear - startYear + 3].innerHTML = "<a href=" + jsonData[orgName][orgYear].link + ">Link</a>";
+    } else if(jsonData[orgName]["years"].includes(orgYear)){
+        myTable.rows[targetRow].cells[targetCol].setAttribute("bgcolor", "blue");
+        myTable.rows[targetRow].cells[endYear - startYear + 1].innerHTML = "--";
+        myTable.rows[targetRow].cells[endYear - startYear + 2].innerHTML = "--";
+        myTable.rows[targetRow].cells[endYear - startYear + 3].innerHTML = "--";
+    }else{
+        myTable.rows[targetRow].cells[targetCol].setAttribute("bgcolor", "red");
+        myTable.rows[targetRow].cells[endYear - startYear + 1].innerHTML = "--";
+        myTable.rows[targetRow].cells[endYear - startYear + 2].innerHTML = "--";
+        myTable.rows[targetRow].cells[endYear - startYear + 3].innerHTML = "--";
+    }
 
 }
 
@@ -90,27 +88,26 @@ function finalCall() {
         for (let i = 0; i < value.years.length; i++) {
             myTable.rows[currRow].cells[value.years[i] - startYear + 1].innerHTML = "Yes";
             var parsedInt = parseInt(value.years[i]);
-            if(value.years.length == i+1){
-                activeCol[key]=value.years[i]-startYear+1;
+            if (value.years.length == i + 1) {
+                activeCol[key] = value.years[i] - startYear + 1;
                 if (parsedInt >= 2016) {
-                    myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor","green");
+                    myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor", "green");
                     myTable.rows[currRow].cells[endYear - startYear + 1].innerHTML = value[value.years[i]].tech;
                     myTable.rows[currRow].cells[endYear - startYear + 2].innerHTML = value[value.years[i]].topics;
                     myTable.rows[currRow].cells[endYear - startYear + 3].innerHTML = "<a href=" + value[value.years[i]].link + ">Link</a>";
                 } else {
-                    myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor","blue");
+                    myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor", "blue");
                     myTable.rows[currRow].cells[endYear - startYear + 1].innerHTML = "--";
                     myTable.rows[currRow].cells[endYear - startYear + 2].innerHTML = "--";
                     myTable.rows[currRow].cells[endYear - startYear + 3].innerHTML = "--"
                 }
             }
-            
+
         }
         currRow++;
     }
     jsonData = sortedObject;
-    //console.log(activeCol);
-    }
+}
 
 async function callME() {
     await fetch("./allDataCombined.json")
