09b06f3526dd436eb60d271ac37884a19b714fe9
RajGM <rajgmsocial19@gmail.com>
2021-01-31 13:24:57 +0530

    search feature testing going on

---
+++ allVar.js
@@ -0,0 +1,8 @@
+
+
+/*
+export default {yearOptions,yearOpt1,yearOpt12,yearOpt3,
+    yearForm,andOr1,andOrOpt1,andOrOpt2,
+    toolForm,andOrFinal,andOrFinalOpt1,andOrFinalOpt2,
+    topicForm,goButton};
+*/
--- index.js
+++ index.js
@@ -11,13 +11,141 @@ let currentActiveColor = "yellow";
 let participatedAndNoInfoColor = "blue";
 let participatedAndInfoColor = "green";
 let notParticipatedColor = "red";
-let yearOpt1 = document.getElementById("yearOpt1");
 
-//it's working
-yearOpt1.onclick = function (){
-    window.alert("Welcome bro");
-    console.log("Alert Bro");
-};
+
+
+
+
+
+
+
+
+let yearOptions = document.getElementById("yearOptions");
+let yearOpt1 = document.getElementById("yearOpt1");
+let yearOpt12 = document.getElementById("yearOpt2");
+let yearOpt3 = document.getElementById("yearOpt3");
+let yearForm = document.getElementById("yearForm");
+let andOr1 = document.getElementById("andOr1");
+let andOrOpt1 = document.getElementById("andOrOpt1");
+let andOrOpt2 = document.getElementById("andOrOpt2");
+let toolForm = document.getElementById("toolForm");
+let andOrFinal = document.getElementById("andOrFinal");
+let andOrFinalOpt1 = document.getElementById("andOrFinalOpt1");
+let andOrFinalOpt2 = document.getElementById("andOrFinalOpt2");
+let topicForm = document.getElementById("topicForm");
+let goButton = document.getElementById("goButton");
+let Query = {
+    YearQ: "At least",
+    Years: "0",
+    andOr1: "OR",
+    Tool: "",
+    andOr2: "OR",
+    Topic: "",
+    /*
+    Tool:{
+        OR:[],
+        AND:[]
+    },
+    Topic:{
+        OR:[],
+        AND:[]
+    }
+    */
+}
+
+yearOpt1.onclick = function () {
+    yearOptions.innerHTML = yearOpt1.innerHTML;
+    Query.YearQ = "at least"
+}
+
+yearOpt2.onclick = function () {
+    yearOptions.innerHTML = yearOpt2.innerHTML;
+    Query.YearQ = "at most"
+}
+
+yearOpt3.onclick = function () {
+    yearOptions.innerHTML = yearOpt3.innerHTML;
+    Query.YearQ = "equal"
+}
+
+yearForm.onchange = function () {
+    Query.Years = yearForm.value;
+}
+
+andOrOpt1.onclick = function () {
+    andOr1.innerHTML = andOrOpt1.innerHTML;
+    Query.andOr1 = andOrOpt1.innerHTML;
+}
+
+andOrOpt2.onclick = function () {
+    andOr1.innerHTML = andOrOpt2.innerHTML;
+    Query.andOr1 = andOrOpt2.innerHTML;
+}
+
+andOrFinalOpt1.onclick = function () {
+    andOrFinal.innerHTML = andOrFinalOpt1.innerHTML;
+    Query.andOr2 = andOrFinalOpt1.innerHTML;
+}
+
+andOrFinalOpt2.onclick = function () {
+    andOrFinal.innerHTML = andOrFinalOpt2.innerHTML;
+    Query.andOr2 = andOrFinalOpt2.innerHTML;
+}
+
+goButton.onclick = function () {
+    console.log(yearForm.value);
+    console.log(toolForm.value);
+    console.log(topicForm.value);
+    console.log(Query);
+    console.log("Go button clicked");
+    //do  a query search here
+
+    for (const [key, value] of Object.entries(jsonData)) {
+        if (value.years.length > 4) {
+            for (let i = 0; i < value.years.length; i++) {
+                if (value.years[i] >= 2016) {
+                    if (value[value.years[i]]["li"] !== "undefined") {
+                        console.log(value[value.years[i]].tech);
+                    }
+                }
+
+            }
+        }
+
+    }
+
+
+}
+
+console.log(Query);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //build dummy 0th row
 function buildCell() {
@@ -44,7 +172,7 @@ function testBuild(totalOrg) {
                 currCell.innerHTML = "Organization";
             } else if (j >= 1 && j <= endYear - startYear) {
                 currCell.innerHTML = "No";
-                currCell.setAttribute("bgcolor",notParticipatedColor);
+                currCell.setAttribute("bgcolor", notParticipatedColor);
                 currCell.addEventListener('click', changeData);
             } else {
                 currCell.innerHTML = " ";
@@ -60,7 +188,7 @@ function changeData(e) {
     let orgName = Object.keys(jsonData)[e.target.parentNode.rowIndex - 1];
     let activeColcell = activeCol[orgName];
     let orgYear = startYear + targetCol - 1;
-    
+
     myTable.rows[targetRow].cells[activeColcell].setAttribute("bgcolor", "white");
     activeCol[orgName] = targetCol;
 
@@ -69,34 +197,34 @@ function changeData(e) {
         myTable.rows[targetRow].cells[endYear - startYear + 1].innerHTML = jsonData[orgName][orgYear].tech;
         myTable.rows[targetRow].cells[endYear - startYear + 2].innerHTML = jsonData[orgName][orgYear].topics;
         myTable.rows[targetRow].cells[endYear - startYear + 3].innerHTML = "<a href=" + jsonData[orgName][orgYear].link + ">Link</a>";
-        if(activeState[orgName]){
+        if (activeState[orgName]) {
             myTable.rows[targetRow].cells[targetCol].innerHTML = orgYear;
             activeState[orgName] = false;
-        }else{
+        } else {
             myTable.rows[targetRow].cells[targetCol].innerHTML = "Yes";
             activeState[orgName] = true;
         }
-    } else if(jsonData[orgName]["years"].includes(orgYear.toString())){
+    } else if (jsonData[orgName]["years"].includes(orgYear.toString())) {
         myTable.rows[targetRow].cells[targetCol].setAttribute("bgcolor", participatedAndNoInfoColor);
         myTable.rows[targetRow].cells[endYear - startYear + 1].innerHTML = "--";
         myTable.rows[targetRow].cells[endYear - startYear + 2].innerHTML = "--";
         myTable.rows[targetRow].cells[endYear - startYear + 3].innerHTML = "--";
-        if(activeState[orgName]){
+        if (activeState[orgName]) {
             myTable.rows[targetRow].cells[targetCol].innerHTML = orgYear;
             activeState[orgName] = false;
-        }else{
+        } else {
             myTable.rows[targetRow].cells[targetCol].innerHTML = "Yes";
             activeState[orgName] = true;
         }
-    }else{
+    } else {
         myTable.rows[targetRow].cells[targetCol].setAttribute("bgcolor", notParticipatedColor);
         myTable.rows[targetRow].cells[endYear - startYear + 1].innerHTML = "--";
         myTable.rows[targetRow].cells[endYear - startYear + 2].innerHTML = "--";
         myTable.rows[targetRow].cells[endYear - startYear + 3].innerHTML = "--";
-        if(activeState[orgName]){
+        if (activeState[orgName]) {
             myTable.rows[targetRow].cells[targetCol].innerHTML = orgYear;
             activeState[orgName] = false;
-        }else{
+        } else {
             myTable.rows[targetRow].cells[targetCol].innerHTML = "No";
             activeState[orgName] = true;
         }
@@ -116,10 +244,10 @@ function finalCall() {
         for (let i = 0; i < value.years.length; i++) {
             myTable.rows[currRow].cells[value.years[i] - startYear + 1].innerHTML = "Yes";
             var parsedInt = parseInt(value.years[i]);
-            if(parsedInt>=2016){
-                myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor",participatedAndInfoColor);
-            }else{
-                myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor",participatedAndNoInfoColor);    
+            if (parsedInt >= 2016) {
+                myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor", participatedAndInfoColor);
+            } else {
+                myTable.rows[currRow].cells[value.years[i] - startYear + 1].setAttribute("bgcolor", participatedAndNoInfoColor);
             }
 
             if (value.years.length == i + 1) {
