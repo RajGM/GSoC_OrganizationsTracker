55d4a68121bc15513bdd59c37ca69949eff47ff5
RajGM <rajgmsocial19@gmail.com>
2021-03-30 01:24:21 +0530

    blank file deleted

--- allVar.js
+++
@@ -1,8 +0,0 @@
-
-
-/*
-export default {yearOptions,yearOpt1,yearOpt12,yearOpt3,
-    yearForm,andOr1,andOrOpt1,andOrOpt2,
-    toolForm,andOrFinal,andOrFinalOpt1,andOrFinalOpt2,
-    topicForm,goButton};
-*/
