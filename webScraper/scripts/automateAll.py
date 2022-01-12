25355e67c43ca8d013a1f462a2cbddbca044ad32
RajGM <rajgmsocial19@gmail.com>
2022-01-13 02:36:41 +0530

    initial data gathering combined in single python file

---
+++ webScraper/scripts/automateAll.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    print("Hello World!")
+
+def main2Test():
+    print("Hello World! main2Test")
+
+if __name__ == "__main__":
+    print(__name__)
+    main()
---
+++ webScraper/scripts/gatherData2009to20.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import re
+import urllib.request
+
+def gather2015Data():
+ for year in range(2009,2016):
+  strYear = str(year)
+  data = urllib.request.urlopen('https://www.google-melange.com/archive/gsoc/'+strYear).read()
+  soup = BeautifulSoup(data,"html.parser")
+  x = 0
+  orgDone = 0
+  f = open(strYear+'Org.txt','w')
+
+  for li in soup.find_all('a'):
+   if x==1 and li.text == "Google Summer of Code":
+    x = 0
+   elif li.text == "Google Summer of Code":
+    x = 1
+   elif li.text == "Privacy Policy" and x==1:
+    x = 3
+    orgDone = 1
+
+   if x==1 and li.text != "Google Summer of Code" and orgDone==0:
+    f.write(li.text+"\n")
+    
+  f.close()
+
+def gather2020Data():
+ for year in range(2016,2021):
+  strYear = str(year)
+  print(strYear)
+  data = urllib.request.urlopen('https://summerofcode.withgoogle.com/archive/'+strYear+'/organizations/').read()
+  soup = BeautifulSoup(data,"html.parser")
+  f = open(strYear+'Org.txt','w')
+  for li in soup.find_all('h4'):
+   print(li.text)
+   f.write(li.text+"\n")  
+  f.close()
+
+gather2015Data()
+gather2020Data()
